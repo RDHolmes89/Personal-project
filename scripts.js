05fd137024a22e985177a7e9d1d26e4e68580ae8
rdholmes89 <rochelleholmes89@gmail.com>
2022-05-10 11:07:49 -0400

    npm installed my google maps api is on my profile and is allowing me to drop markers done using external js and css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,66 @@
-//WAITING FOR YOU WHEN READY//
+
+function initMap() {
+    //Map options
+    var options = {
+        zoom: 8,
+        center: { lat: 42.3601, lng: -71.0589 },
+    }
+    //New map
+    var map = new
+        google.maps.Map(document.getElementById('map'), options);
+    //Listen for click on map addList
+    google.maps.event.addListener(map, 'click', function(e){
+        addMarker({coords:e.latLng});
+    });
+//  IMAGINE when you click it and trigger the event will save and store the lat and longitude in the markers array?// //WOULD I NEED A SAVE BUTTON OR FORM?
+
+    // create array of markers
+    //THIS ARRAY CAN BE STORED IN A DATABASE//
+    //WILL PROBABLY NEED TO CHANGE THE COORDS AND CONTENT SAVED TO BE BASED ON WHAT IS CLICKED
+
+    //maybe will plug in coords etc as the values for my form if i need to create one//
+    //PROBABLY need to create a varable to store what the content is acutually based on long and lats (i.e. content: plug in whatever var you chose)
+//might use flagiconimage for ending points only!
+    var markers = [
+        {
+            coords: { lat: 42.5195, lng: -70.8967 },
+            iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            content: 'Salem, MA'
+        },
+        { coords: { lat: 42.2832, lng: -71.1265 },
+    content: 'Roslindale, MA'
+    },
+    { coords: { lat: 42.1015, lng: -72.5898},
+    content: 'Springfield, MA'
+    }
+    ];
+
+    //Loop through markers
+    for(var i=0;i < markers.length; i++ ){
+        //ADD MARKER
+        addMarker(markers[i]);
+    }
+    
+    //Add Marker Function
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        });
+        //check for custom icon
+        if (props.iconImage) {
+            //Set icon image
+            marker.setIcon(props.iconImage);
+        }
+        // Check content
+        if(props.content){
+            var infoWindow = new google.maps.InfoWindow( {
+                content: props.content
+            });
+  // maybe add info to bottom addlistener function to save the location of where you dropped your pin//
+            marker.addListener('click', function(){
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+}
