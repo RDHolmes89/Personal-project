7eff8cf0a49d6675febfba6b66a177e068832b38
rdholmes89 <rochelleholmes89@gmail.com>
2022-05-11 18:33:27 -0400

    app.js server port is set up

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const methodOverride = require('method-override');
+const routes = require('')
+
+//set up ejs files to be viewed
+app.set('view engine', 'ejs');
+
+//app.use() will replace the app.get()req,res ('') which youll probably use first
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
