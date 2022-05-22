904784d3689b86d195b650303e7dcec8d9262f69
rdholmes89 <rochelleholmes89@gmail.com>
2022-05-22 18:26:29 -0400

    files converted to ejs and partials are created and implimented, all routes im getting are rendering and all link paths have been updated, think my javascripts page isnt connecting for some reason cause map isnot displaying

--- app.js
+++ app.js
@@ -3,14 +3,40 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 const path = require('path');
-const methodOverride = require('method-override');
-const routes = require('')
+// const methodOverride = require('method-override');
+// const routes = require('')
+
 
 //set up ejs files to be viewed
 app.set('view engine', 'ejs');
 
-//app.use() will replaces the app.get()req,res ('') which youll probably use first
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTES
+//app.use() will replaces the app.get()
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+app.get('/create', (req, res) => {
+    res.render('pages/create');
+});
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+app.get('/profile', (req, res) => {
+    res.render('pages/profile');
+});
+app.get('/maps', (req, res) => {
+    res.render('pages/maps');
+});
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+});
+app.get('/contact', (req, res) => {
+    res.render('pages/contact');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
-})
+});
